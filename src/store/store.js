52cfe92654e29a157f9e506c2861cd9baa7f3284
Maxim Kuzin <iskait04@gmail.com>
2022-11-17 18:14:50 +0300

    feat: Добавление vue-router (#10)

--- src/store/store.js
+++ src/store/store.js
@@ -1,62 +1,9 @@
 import { createStore } from "vuex";
-
+import { setModule } from "./modules/setModule";
+import { elevatorsModule } from "./modules/elevatorsModule";
 export default createStore({
-  state: () => ({
-    slides: 4,
-    stages: 9,
-    elevators: [
-      {
-      id: 0,
-      free: true,
-      done: false,
-      currentStage: 1,
-      level: 1
-    },
-    {
-      id: 1,
-      free: true,
-      done: false,
-      currentStage: 1,
-      level: 1
-    },
-    {
-      id: 2,
-      free: true,
-      done: false,
-      currentStage: 1,
-      level: 1
-    },
-    {
-      id: 3,
-      free: true,
-      done: false,
-      currentStage: 1,
-      level: 1
-    },
-  ],
-    queue: [],
-  }),
-  mutations: {
-    setQueue(state, payload) {
-      !state.queue.includes(payload) && 
-      !state.elevators.every(elevator=>elevator.currentStage === payload) && 
-      (state.queue = state.queue.concat(payload));
-    },
-    startElevator(state, {id, level}) {
-      const elevator = state.elevators.find(elevator=>elevator.id === id);
-      elevator.level = level;
-      elevator.free = false;
-      state.queue.shift();
-    },
-    stopElevator(state, { id }) {
-      const elevator = state.elevators.find(elevator=>elevator.id === id);
-      elevator.currentStage = elevator.level;
-      elevator.done = true;
-    },
-    readyElevator(state, { id }) {
-      const elevator = state.elevators.find(elevator=>elevator.id === id);
-      elevator.done = false;
-      elevator.free = true;
-    }
-  },
-});
+  modules: {
+    setModule,
+    elevatorsModule
+  }
+})
