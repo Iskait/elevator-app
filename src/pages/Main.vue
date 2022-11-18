9fe52025c65c4c95d7f69dfb7bb9d92828bbf19f
iskait <iskait04@gmail.com>
2022-11-18 12:55:30 +0300

    Рефактор компонента Main #17

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,15 +1,9 @@
-<template>
-  <lift-shaft></lift-shaft>
-  <shaft-control></shaft-control>
-</template>
-
-<script>
-import liftShaft from "@/components/liftShaft.vue";
+<script setup lang="ts">
+import LiftShaft from "@/components/LiftShaft.vue";
 import ShaftControl from "@/components/ShaftControl.vue";
-
-export default {
-  components: { liftShaft, ShaftControl },
-};
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+  <LiftShaft />
+  <ShaftControl />
+</template>
